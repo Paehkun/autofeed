3e4bc2e2adcf28cc4428b7f4115c7bf36611f552
Mohamad Faiz Bin Rosdin <faizrosdin0123@gmail.com>
2025-04-27 00:02:43 +0800

    add camera module

--- lib/screen/monitor.dart
+++ lib/screen/monitor.dart
@@ -1,17 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mjpeg/flutter_mjpeg.dart';
 
 class Monitor extends StatelessWidget {
   const Monitor({super.key});
 
   @override
   Widget build(BuildContext context) {
-    Title(
-      color: Colors.black,
-      child: const Text(
-        "monitor page",
-        style: TextStyle(color: Colors.black),
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(16),
+              child: const Text(
+                "Monitor Page",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            Expanded(
+              child: Center(
+                child: Container(
+                  width: 400,
+                  height: 340,
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(12),
+                    border: Border.all(color: Colors.grey, width: 4),
+                  ),
+                  child: const Mjpeg(
+                    stream: 'http://192.168.0.25/stream',
+                    isLive: true,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
-    return const Placeholder();
   }
 }
