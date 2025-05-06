ea8287b4b154222cd324642b552f91f4ac66ff65
Mohamad Faiz Bin Rosdin <faizrosdin0123@gmail.com>
2025-05-06 22:03:44 +0800

    add singlechildscrollview

--- lib/screen/report.dart
+++ lib/screen/report.dart
@@ -33,7 +33,7 @@ class _ReportScreenState extends State<ReportScreen> {
     final snapshot =
         await _database.child('users/${_currentUser.uid}/food_usage').get();
 
-    if (snapshot.exists) {
+    if (snapshot.exists && mounted) {
       Map<dynamic, dynamic> logs = snapshot.value as Map<dynamic, dynamic>;
 
       // Reset arrays before calculating
@@ -107,176 +107,221 @@ class _ReportScreenState extends State<ReportScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Container(
-        color: Colors.grey[300],
-        child: Column(
-          children: [
-            const Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Text(
-                "Report",
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Text(
-                    "Select View:",
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          // Wrap the content inside the scroll view
+          child: Container(
+            color: Colors.white,
+            child: Column(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Text(
+                    "Report",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                   ),
-                  DropdownButton<String>(
-                    value: selectedFilter,
-                    items: ["Week", "Month", "Year"]
-                        .map((String value) => DropdownMenuItem<String>(
-                              value: value,
-                              child: Text(value),
-                            ))
-                        .toList(),
-                    onChanged: (newValue) {
-                      if (newValue != null) {
-                        setState(() {
-                          selectedFilter = newValue;
-                          updateChartData();
-                        });
-                      }
-                    },
-                  ),
-                ],
-              ),
-            ),
-            // White Container for the Chart with increased size
-            Container(
-              height: MediaQuery.of(context).size.height * 0.6, // Taller
-              width: MediaQuery.of(context).size.width * 0.9, // Wider
-              decoration: BoxDecoration(
-                color: Colors.white,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.1), // Shadow color
-                    spreadRadius: 10, // Spread of the shadow
-                    blurRadius: 10, // How much the shadow is blurred
-                    offset: const Offset(3, 5), // Position of the shadow (x, y)
-                  ),
-                ],
-                borderRadius: BorderRadius.circular(25),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: LineChart(
-                  LineChartData(
-                    titlesData: FlTitlesData(
-                      leftTitles: AxisTitles(
-                        sideTitles: SideTitles(
-                          showTitles: true,
-                          getTitlesWidget: (value, meta) {
-                            if (selectedFilter == "Week") {
-                              return Text(
-                                '${value.toInt()} grams',
-                                style: const TextStyle(fontSize: 10),
-                              );
-                            } else {
-                              return Text(
-                                '${value.toInt()} kg', // Show in kg for Month/Year
-                                style: const TextStyle(fontSize: 10),
-                              );
-                            }
-                          },
-                          reservedSize: 30,
-                        ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        "Select View:",
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold),
                       ),
-                      bottomTitles: AxisTitles(
-                        sideTitles: SideTitles(
-                          showTitles: true,
-                          getTitlesWidget: (value, meta) {
-                            if (selectedFilter == "Week") {
-                              // Weekdays (Mon, Tue, Wed, ...)
-                              List<String> weekdays = [
-                                "Mon",
-                                "Tue",
-                                "Wed",
-                                "Thu",
-                                "Fri",
-                                "Sat",
-                                "Sun"
-                              ];
-                              return Text(
-                                weekdays[value.toInt()],
-                                style: const TextStyle(fontSize: 10),
-                              );
-                            } else if (selectedFilter == "Month") {
-                              // Weeks of the month (Week 1, Week 2, Week 3, Week 4)
-                              return Text(
-                                "Week ${value.toInt() + 1}",
-                                style: const TextStyle(fontSize: 10),
-                              );
-                            } else if (selectedFilter == "Year") {
-                              // Month names (Jan, Feb, Mar, ...)
-                              List<String> months = [
-                                "Jan",
-                                "Feb",
-                                "Mar",
-                                "Apr",
-                                "May",
-                                "Jun",
-                                "Jul",
-                                "Aug",
-                                "Sep",
-                                "Oct",
-                                "Nov",
-                                "Dec"
-                              ];
-                              int monthIndex = value.toInt();
-                              return Text(
-                                months[monthIndex],
-                                style: const TextStyle(fontSize: 10),
-                              );
-                            } else {
-                              return Text(
-                                "M${value.toInt()}",
-                                style: const TextStyle(fontSize: 10),
-                              );
-                            }
-                          },
-                        ),
-                      ),
-                      rightTitles: const AxisTitles(
-                        sideTitles: SideTitles(showTitles: false),
-                      ),
-                      topTitles: const AxisTitles(
-                        sideTitles: SideTitles(showTitles: false),
-                      ),
-                    ),
-                    borderData: FlBorderData(show: false),
-                    lineBarsData: [
-                      LineChartBarData(
-                        spots: chartData,
-                        isCurved: false,
-                        color: Colors.blue,
-                        barWidth: 3,
-                        isStrokeCapRound: true,
-                        dotData: const FlDotData(show: true),
-                        belowBarData: BarAreaData(show: false),
+                      DropdownButton<String>(
+                        value: selectedFilter,
+                        items: ["Week", "Month", "Year"]
+                            .map((String value) => DropdownMenuItem<String>(
+                                  value: value,
+                                  child: Text(value),
+                                ))
+                            .toList(),
+                        onChanged: (newValue) {
+                          if (newValue != null) {
+                            setState(() {
+                              selectedFilter = newValue;
+                              updateChartData();
+                            });
+                          }
+                        },
                       ),
                     ],
                   ),
                 ),
-              ),
-            ),
-            // Summary text
-            Expanded(
-              child: Center(
-                child: Text(
-                  'Total food usage in $selectedFilter view',
-                  style: const TextStyle(
-                      fontSize: 18, fontWeight: FontWeight.bold),
+                // White Container for the Chart with increased size
+                Container(
+                  height: 400, // Set a fixed height for the chart
+                  width: MediaQuery.of(context).size.width * 0.9, // Wider
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.1), // Shadow color
+                        spreadRadius: 10, // Spread of the shadow
+                        blurRadius: 10, // How much the shadow is blurred
+                        offset:
+                            const Offset(3, 5), // Position of the shadow (x, y)
+                      ),
+                    ],
+                    borderRadius: BorderRadius.circular(25),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: LineChart(
+                      LineChartData(
+                        titlesData: FlTitlesData(
+                          leftTitles: AxisTitles(
+                            sideTitles: SideTitles(
+                              showTitles: true,
+                              getTitlesWidget: (value, meta) {
+                                if (selectedFilter == "Week") {
+                                  return Text(
+                                    '${value.toInt()} grams',
+                                    style: const TextStyle(fontSize: 10),
+                                  );
+                                } else {
+                                  return Text(
+                                    '${value.toInt()} kg', // Show in kg for Month/Year
+                                    style: const TextStyle(fontSize: 10),
+                                  );
+                                }
+                              },
+                              reservedSize: 30,
+                            ),
+                          ),
+                          bottomTitles: AxisTitles(
+                            sideTitles: SideTitles(
+                              showTitles: true,
+                              getTitlesWidget: (value, meta) {
+                                if (selectedFilter == "Week") {
+                                  // Weekdays (Mon, Tue, Wed, ...)
+                                  List<String> weekdays = [
+                                    "Mon",
+                                    "Tue",
+                                    "Wed",
+                                    "Thu",
+                                    "Fri",
+                                    "Sat",
+                                    "Sun"
+                                  ];
+                                  return Text(
+                                    weekdays[value.toInt()],
+                                    style: const TextStyle(fontSize: 10),
+                                  );
+                                } else if (selectedFilter == "Month") {
+                                  // Weeks of the month (Week 1, Week 2, Week 3, Week 4)
+                                  return Text(
+                                    "Week ${value.toInt() + 1}",
+                                    style: const TextStyle(fontSize: 10),
+                                  );
+                                } else if (selectedFilter == "Year") {
+                                  // Month names (Jan, Feb, Mar, ...)
+                                  List<String> months = [
+                                    "Jan",
+                                    "Feb",
+                                    "Mar",
+                                    "Apr",
+                                    "May",
+                                    "Jun",
+                                    "Jul",
+                                    "Aug",
+                                    "Sep",
+                                    "Oct",
+                                    "Nov",
+                                    "Dec"
+                                  ];
+                                  int monthIndex = value.toInt();
+                                  return Text(
+                                    months[monthIndex],
+                                    style: const TextStyle(fontSize: 10),
+                                  );
+                                } else {
+                                  return Text(
+                                    "M${value.toInt()}",
+                                    style: const TextStyle(fontSize: 10),
+                                  );
+                                }
+                              },
+                            ),
+                          ),
+                          rightTitles: const AxisTitles(
+                            sideTitles: SideTitles(showTitles: false),
+                          ),
+                          topTitles: const AxisTitles(
+                            sideTitles: SideTitles(showTitles: false),
+                          ),
+                        ),
+                        borderData: FlBorderData(show: false),
+                        lineBarsData: [
+                          LineChartBarData(
+                            spots: chartData,
+                            isCurved: false,
+                            color: Colors.blue,
+                            barWidth: 3,
+                            isStrokeCapRound: true,
+                            dotData: const FlDotData(show: true),
+                            belowBarData: BarAreaData(show: false),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
                 ),
-              ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Center(
+                    child: Text(
+                      'Total food usage in $selectedFilter view',
+                      style: const TextStyle(
+                          fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+                Container(
+                  height:
+                      200, // Adjusted height to fit schedule and toggle only
+                  width: 350,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.1), // Shadow color
+                        spreadRadius: 10, // Spread of the shadow
+                        blurRadius: 10, // How much the shadow is blurred
+                        offset:
+                            const Offset(3, 5), // Position of the shadow (x, y)
+                      ),
+                    ],
+                    borderRadius: BorderRadius.circular(25),
+                  ),
+                  child: const Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(height: 20),
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 20.0), // Add horizontal padding
+                        child: Center(
+                          child: Text(
+                            "Feeding Log",
+                            style: TextStyle(
+                              fontSize: 15.0,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
